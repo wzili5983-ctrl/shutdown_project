666fdc965e19aac505c79d43b802b653f6170d99
wzili5983-ctrl <wzili5983@gmail.com>
2026-09-16 17:47:44 +0800

    1、第4版程序； 2、修改主要用于测试；

--- DailyPower.bat
+++ DailyPower.bat
@@ -2,6 +2,6 @@
 setlocal
 title Daily Power Schedule
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0DailyPower.ps1"
-echo "Program ends!"
+echo "press any key to exit program."
 pause
 endlocal
